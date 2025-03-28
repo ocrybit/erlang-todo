1951fdb2f5da890568d1f1a8f4b6c2917dc26f1e
Tomoya Nagasawa <ocrybit@gmail.com>
2025-03-28 19:09:04 +0400

    make it persistent

--- user_default.erl
+++ user_default.erl
@@ -1,9 +1,21 @@
 -module(user_default).
 -export([start/0]).
 
+save_todos({Count, Todos}) ->
+    Bin = term_to_binary({Count, Todos}),
+    ok = file:write_file("todos.db", Bin).
+
+load_todos() ->
+    case file:read_file("todos.db") of
+        {ok, Bin} ->
+            binary_to_term(Bin);
+        {error, _} ->
+            {0, []}  % default if file doesn't exist
+    end.
+
 start() ->
     io:format("Todo app started!~n"),
-    State = {0, []},
+    State = load_todos(),
     start(State).
 
 start({ Count, Todos }) ->
@@ -20,14 +32,17 @@ start({ Count, Todos }) ->
 		{Count, Filtered};
 	    ["complete", IdStr] ->
 		{Id, _} = string:to_integer(IdStr),
-		Updated = lists:map(
-			    fun
-				({I, T, _}) when I =:= Id -> {I, T, true};
-				(Item) -> Item
+		Updated
+		    = lists:map(
+			fun
+			    ({I, T, _}) when I =:= Id -> {I, T, true};
+			    (Item) -> Item
 			    end,
-			    Todos
-			   ),
+			Todos
+		       ),
 		{Count, Updated};
+	    ["clear"] ->
+		{Count, []};
 	    _ ->
 		io:format("invalid~n"),
 		{Count, Todos}
@@ -37,5 +52,6 @@ start({ Count, Todos }) ->
       fun({Id, T, Done}) -> io:format(" - [~p] ~s (~p) ~n", [Id, T, Done]) end,
       lists:reverse(NewTodos)
      ),
+    save_todos({NewCount, NewTodos}),
     start({ NewCount, NewTodos }).
 
